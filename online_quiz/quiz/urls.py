17e1858f44d5e9711f9be37f226b323cc12f7641
Vavola <97082438+Vavola@users.noreply.github.com>
2025-02-22 13:36:46 +0300

    Delete online_quiz directory
    лдж

--- online_quiz/quiz/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import QuizListView, QuizCreateView, QuizDetailView, dashboard
-
-urlpatterns = [
-    path('', QuizListView.as_view(), name='quiz_list'),
-    path('create/', QuizCreateView.as_view(), name='quiz_create'),
-    path('<int:pk>/', QuizDetailView.as_view(), name='quiz_detail'),
-    path('dashboard/', dashboard, name='dashboard'),
-]
